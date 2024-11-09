7ed0bb6f52839d6d5a5f3758dd4332c9b0999245
Codetron <34787157+Codetron@users.noreply.github.com>
2024-11-09 18:40:34 +0100

    Added sample app

---
+++ simple-spring-boot/src/test/java/it/codetrondev/simple_spring_boot/SimpleSpringBootApplicationTests.java
@@ -0,0 +1,39 @@
+package it.codetrondev.simple_spring_boot;
+
+import it.codetrondev.simple_spring_boot.controller.HelloController;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@WebMvcTest()
+class SimpleSpringBootApplicationTests {
+
+	@Value("app.controller.get.message")
+	private String helloMessage;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	@SneakyThrows
+	void testHelloController() {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/hello-world"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.jsonPath("$").value(helloMessage));
+	}
+
+
+}
