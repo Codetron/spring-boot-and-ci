559935e919bb3d5bc37301dead2bbcb316042992
Codetron <34787157+Codetron@users.noreply.github.com>
2024-11-09 18:39:19 +0100

    added git ignore

---
+++ simple-spring-boot/src/main/java/it/codetrondev/simple_spring_boot/controller/HelloController.java
@@ -0,0 +1,22 @@
+package it.codetrondev.simple_spring_boot.controller;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class HelloController {
+
+    @Value("app.controller.get.message")
+    private String helloMessage;
+    @GetMapping(value = "hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok(helloMessage);
+    }
+}
